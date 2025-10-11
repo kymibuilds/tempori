690203c95208327e9ab4c724251ad248cfcdddeb
kymibuilds <kymibuilds@gmail.com>
2025-10-11 21:13:04 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function Home() {
+  return (
+    <div className="text-red-50">
+      <Button>something</Button>
+    </div>
+  );
+}
+
+export default Home;
